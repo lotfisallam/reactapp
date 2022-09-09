afbd4c8d7eaf03ade8e66c83bef796936f41a3ef
lotfi sallam <qwe199911@gmail.com>
2022-09-09 11:10:31 +0300

    9/9 mod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Child  from './Child';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+ //arow function 
+  Test=()=>{
+    console.log('am i here ?');
+  }
+  Test2(){
+    console.log('sds');
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      hello react
+      <Child/> 
+      {this.Test()}
+      {this.Test2()}
+      <button>click</button>
+     </div>
+     );
+  }
+  
 }
 
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,12 @@
+import React ,{Component}from "react";
+
+class Child extends Component {
+    render(){
+        return(
+           <div>
+            i am in child comp
+           </div>
+        );
+        }
+    }
+    export default Child;
